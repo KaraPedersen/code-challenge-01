d2c2da5ebf49edd83faa9cf4a8f952b9581dd34c
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-04-26 22:10:01 -0700

    testing yay

--- src/array-map.js
+++ src/array-map.js
@@ -5,3 +5,16 @@ export function doubleNumbers(arr) {
   });
 
 }
+
+export function stringyNumbers(arr) {
+  return arr.map(item => {
+    const stringy = item + '';
+    return stringy;
+  });
+}
+
+export function capitalizeNames(arr) {
+  return arr.charAt(0).toUpperCase() + arr.slice(1);
+}
+capitalizeNames(input);
+
--- src/array-map.test.js
+++ src/array-map.test.js
@@ -1,8 +1,20 @@
-import { doubleNumbers } from './array-map';
+import { doubleNumbers, stringyNumbers, capitalizeNames } from './array-map';
 
 test('it doubles the numbers', () => {
   const input = [2, 5, 100];
   const actual = doubleNumbers(input);
   expect(actual).toEqual([4, 10, 200]);
 
+});
+
+test('it turns the array to a string', () => {
+  const input = [2, 5, 100];
+  const actual = stringyNumbers(input);
+  expect(actual).toEqual(['2', '5', '100']);
+});
+
+test('capitalize each of an array of names', () => {
+  const input = ['john', 'JACOB', 'jinGleHeimer', 'schmidt'];
+  const actual = capitalizeNames(input);
+  expect actual.toEqual(['John', 'Jacob', 'Jingleheimer', 'Schmidt'])
 });
